141abbbcd419adfbee349a729ccd05fef4608b80
gusmontana <gmt015@gmail.com>
2024-07-12 20:51:08 -0400

    homepage finished

---
+++ app/Components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer>
+      <hr className={"h-px my-6 dark:bg-neutral-700 bg-neutral-300 border-0"}/>
+      <div className={"flex flex-row justify-between"}>
+        <p className={"text-neutral-400 text-sm italic"}>Gustave Montana</p>
+        <p className={"text-neutral-400 text-sm italic"}>2024</p>
+      </div>
+    </footer>
+  );
+}
---
+++ app/Components/ProjectCard.tsx
@@ -0,0 +1,20 @@
+interface ProjectCardProps {
+  title: string;
+  description: string;
+  image: string;
+  path: string;
+}
+
+export default function ProjectCard({ title, description, image, path }: ProjectCardProps) {
+  return (
+    <div>
+      <div className={"mb-3 p-2"}>
+        <h1 className={"text-2xl font-bold"}>{title}</h1>
+        <p className={"text-sm h-16"}>{description}</p>
+      </div>
+      <a href={path}>
+        <img src={image} alt={title} height={"315"} className={"max-w-full mx-auto rounded-md img-hover-zoom"}/>
+      </a>
+    </div>
+  );
+}
