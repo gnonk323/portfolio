629ae71fc885529a32f58aa3d2b03172c3cbeefa
gusmontana <gmt015@gmail.com>
2024-07-24 12:44:01 -0400

    fixed apostrophe for real this time

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className={"lg:ml-64 lg:p-10 p-6 min-h-screen"}>
         <div>
           <TypeAnimation
-            sequence={["Hi, I&apos;m Gustave Montana!"]}
+            sequence={["Hi, I'm Gustave Montana!"]}
             speed={30}
             repeat={1}
             className={"lg:text-7xl text-xl font-mono"}
@@ -19,7 +19,7 @@ export default function Home() {
         <hr className={"h-px my-6 dark:bg-neutral-700 bg-neutral-300 border-0"}/>
         <div>
           <p className="text-xl">
-            I'm a Senior studying Computer Sciecne and Data Science at <a href={"https://www.wpi.edu/academics/departments/computer-science"} target={"_blank"} className={"underline decoration-dotted lg:hover:text-neutral-400 transition-colors cursor-pointer"}>Worcester Polytechnic Institute</a>, with a passion for UI/UX design and front-end development.
+            I&apos;m a Senior studying Computer Sciecne and Data Science at <a href={"https://www.wpi.edu/academics/departments/computer-science"} target={"_blank"} className={"underline decoration-dotted lg:hover:text-neutral-400 transition-colors cursor-pointer"}>Worcester Polytechnic Institute</a>, with a passion for UI/UX design and front-end development.
           </p>
         </div>
         <hr className={"h-px my-6 dark:bg-neutral-700 bg-neutral-300 border-0"}/>
