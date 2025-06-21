f78d7d80e4772291b84aff895087dfc06ad985a1
Gustave Montana <77447364+gnonk323@users.noreply.github.com>
2025-06-21 11:04:42 -0600

    footer padding hotfix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="lg:ml-64 lg:px-24 mb-10">
+    <footer className="lg:ml-64 lg:px-24 px-6 mb-10">
       <hr className={"h-px my-6 dark:bg-neutral-700 bg-neutral-300 border-0"}/>
       <div className={"flex flex-row justify-between"}>
         <p className={"text-neutral-400 text-sm italic"}>Gustave Montana</p>
